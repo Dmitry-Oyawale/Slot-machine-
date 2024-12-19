019f147f7e207f13088bdfdedfbda4fe8aa6c5d8
Dmitry-Oyawale <laikevin279@gmail.com>
2024-12-19 07:47:13 -0500

    added arrays to simulate the spin

--- project.js
+++ project.js
@@ -12,6 +12,25 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
+}
+
+const SYMBOL_VALUES = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
+}
+
+//SYMBOLS_COUNT["A"] -> 2
+
 const deposit = () => {
     while (true) {
             const depositAmount = prompt("Enter a deposit amount: ");
@@ -42,6 +61,80 @@ const getNumberOfLines = () => {
      
 }; 
 
-const depositAmount = deposit();
+const getBet = (balance, lines) => {
+    while (true) {
+            const bet  = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, try again.")
+        }
+        else {
+            return numberBet; 
+        }
+    }
+};
+
+const spin = () => {
+    const symbols = [];
+    //in js, can change what's inside array even if it's a constant
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+    //console.log(symbols);
+
+    const reels = [];
+    for (let i = 0; i < COLS; i++) {
+        reels.push([]);
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+            const selectedSymbol = reelSymbols[randomIndex]; 
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+
+    return reels;
+};
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i])
+        }
+    }
+    return rows;
+}
+
+const printRows = (rows) => {
+    for (const row of rows) {
+        //let rowString = "A | B | C";
+        let rowString = "";
+        for (const [i, symbol] of row.entries()) {
+            //above loop loops through both index and symbol of this:  
+            // ["A", "B", "C"]
+            rowString += symbol
+            if (i != rows.length - 1) {
+                rowString += " | "
+            }
+        }
+        console.log(rowString);
+    }
+}
+
+//console.log(reels);
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+//console.log(reels);
+//console.log(rows);
+printRows(rows);
 //console.log(depositAmount);
